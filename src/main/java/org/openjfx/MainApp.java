ce417d41d1dfadc567532c540da597db407d59ac
Jari-Matti Mäkelä <jmjm@iki.fi>
2019-02-02 18:42:02 +0200

    ...

--- src/main/java/org/openjfx/MainApp.java
+++ src/main/java/org/openjfx/MainApp.java
@@ -31,6 +31,7 @@ public class MainApp extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        if (args.length == 1 && args[0].equals("--test")) return;
         launch(args);
     }
 
