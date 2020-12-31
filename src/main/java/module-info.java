c73989ebadadece45ad2b956b2dc600e3735895f
Jari-Matti Mäkelä <jmjm@iki.fi>
2020-12-31 17:49:22 +0200

    -

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+// declarate module imports and exports here
+// see: https://tech.utugit.fi/soft/tools/lectures/tko8971/build/jpms/
+
+module fi.utu.tech.gui.javafx {
+  //requires java.desktop;
+  requires transitive javafx.base;
+  requires transitive javafx.fxml;
+  requires transitive javafx.controls;
+  requires transitive javafx.graphics;
+  requires transitive javafx.media;
+  requires transitive javafx.web;
+  exports fi.utu.tech.gui.javafx;
+}
---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,12 @@
+package fi.utu.tech.hotreload;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DummyTest {
+    @Test
+    public void dummy() throws Exception {
+        assertTrue(true);
+    }
+}
