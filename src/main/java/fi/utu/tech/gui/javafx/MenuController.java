8c7c7302c31e7a1d7eaca63a480cf4120a60a4d3
nsjude <nsjude@utu.fi>
2022-04-01 11:11:48 +0300

    Controllers separated

---
+++ src/main/java/fi/utu/tech/gui/javafx/MenuController.java
@@ -0,0 +1,41 @@
+package fi.utu.tech.gui.javafx;
+
+import java.io.IOException;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class MenuController {
+
+	@FXML
+	private Button start;
+
+	// siirry stageen laivojen asettamista varten
+	// jos lauta asetetaan johonkin olioon/muuttujaan niin ehkä tää voi vaan
+	// päivittyä?
+	// ja tyyliin kun molemmat laudat on asetettu niin tulee nkyviin nappi et peli
+	// alkaa
+	@FXML
+	public void switchToBoardCreation(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("board.fxml"));
+		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	// sulje sovellus
+	@FXML
+	protected void handleExitButton(ActionEvent event) {
+		System.out.println("Closing app.");
+		Platform.exit();
+	}
+
+}
