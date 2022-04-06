4c4c845f2ec576fbd39bf9deb742b181373bfc26
nsjude <nsjude@utu.fi>
2022-04-06 17:51:05 +0300

    jou

--- src/main/java/fi/utu/tech/gui/javafx/SettingsController.java
+++ src/main/java/fi/utu/tech/gui/javafx/SettingsController.java
@@ -1,7 +1,6 @@
 package fi.utu.tech.gui.javafx;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -19,14 +18,14 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class SettingsController {
-	
+
 	private Lauta lauta = new Lauta();
 	private Pelaaja player1 = new Pelaaja();
 	private Pelaaja player2 = new Pelaaja();
-		
+
 	@FXML
 	private MenuButton laudanKoko;
-	
+
 	@FXML
 	private MenuItem item10;
 	@FXML
@@ -39,16 +38,16 @@ public class SettingsController {
 	private MenuItem item6;
 	@FXML
 	private MenuItem item5;
-	
+
 	@FXML
 	private TextField p1;
-	
+
 	@FXML
 	private TextField p2;
-	
+
 	@FXML
 	private Button valmis;
-	
+
 	@FXML
 	private Spinner<Integer> lta;
 	@FXML
@@ -61,35 +60,32 @@ public class SettingsController {
 	private Spinner<Integer> h;
 	@FXML
 	private Label ruudut;
-	
+
 	private String koko;
 
-		
 	public void initialize() {
 		ruudut.setText("0");
-		ValueFactory<Integer> valuelta = new ValueFactory<Integer>(lta,5,1,ruudut);
+		ValueFactory<Integer> valuelta = new ValueFactory<Integer>(lta, 5, 1, ruudut);
 		lta.setValueFactory(valuelta);
-		ValueFactory<Integer> valuetl = new ValueFactory<Integer>(tl,4,2,ruudut);
+		ValueFactory<Integer> valuetl = new ValueFactory<Integer>(tl, 4, 2, ruudut);
 		tl.setValueFactory(valuetl);
-		ValueFactory<Integer> valuer = new ValueFactory<Integer>(r,3,3,ruudut);
+		ValueFactory<Integer> valuer = new ValueFactory<Integer>(r, 3, 3, ruudut);
 		r.setValueFactory(valuer);
-		ValueFactory<Integer> valuesv = new ValueFactory<Integer>(sv,2,4,ruudut);
+		ValueFactory<Integer> valuesv = new ValueFactory<Integer>(sv, 2, 4, ruudut);
 		sv.setValueFactory(valuesv);
-		ValueFactory<Integer> valueh = new ValueFactory<Integer>(h,2,5,ruudut);
+		ValueFactory<Integer> valueh = new ValueFactory<Integer>(h, 2, 5, ruudut);
 		h.setValueFactory(valueh);
 	}
-	
-@FXML
-void laudanKokoValittu(ActionEvent event) {
-	koko = ((MenuItem) event.getSource()).getText();
-	laudanKoko.setText(koko+"x"+koko);
-	lauta.setKoko(Integer.parseInt(koko));
-	System.out.println("Laudan koko: " + lauta.getKoko());
-	ruudut.setText(String.valueOf(lauta.getKoko()*lauta.getKoko()/2));
-}
-	
-	
-	
+
+	@FXML
+	void laudanKokoValittu(ActionEvent event) {
+		koko = ((MenuItem) event.getSource()).getText();
+		laudanKoko.setText(koko + "x" + koko);
+		lauta.setKoko(Integer.parseInt(koko));
+		System.out.println("Laudan koko: " + lauta.getKoko());
+		ruudut.setText(String.valueOf(lauta.getKoko() * lauta.getKoko() / 2));
+	}
+
 	@FXML
 	void paivitaRuudut(MouseEvent event) {
 		System.out.println("klikattu");
@@ -97,42 +93,37 @@ void laudanKokoValittu(ActionEvent event) {
 		Spinner<Integer> kohde = (Spinner<Integer>) event.getSource();
 		ValueFactory<Integer> factory = (ValueFactory<Integer>) kohde.getValueFactory();
 	}
-	
+
 	@FXML
 	void valmisKlikattu(ActionEvent event) throws IOException {
-		//välitettävät tiedot
+		// välitettävät tiedot
 //		player1.setNimi(p1.getText());
 //		player2.setNimi(p2.getText());
-	
+
 		System.out.println(lta.getValue());
 		System.out.println(r.getValue());
-	
-	//
-		
-		
-		
-			FXMLLoader loader=new FXMLLoader(getClass().getResource("board.fxml"));
-			Parent root = loader.load();
-			
-			//välitetään tiedot
-			setShipsController shipsController=loader.getController();
-			//lauta
-			shipsController.displayLauta(Integer.valueOf(koko));
-			//laivat
-			shipsController.createLentotukialus(lta.getValue(),Integer.valueOf(koko) );
-			shipsController.createTaistelulaiva(tl.getValue(),Integer.valueOf(koko) );
-			shipsController.createRisteilija(r.getValue(),Integer.valueOf(koko) );
-			shipsController.createSukellusvene(sv.getValue(),Integer.valueOf(koko) );
-			shipsController.createHavittaja(h.getValue(),Integer.valueOf(koko) );
-			
-			Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-			Scene scene = new Scene(root);
-			stage.setScene(scene);
-			stage.show();
-		
-		
-		
+
+		//
+
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("board.fxml"));
+		Parent root = loader.load();
+
+		// välitetään tiedot
+		setShipsController shipsController = loader.getController();
+		// lauta
+		shipsController.displayLauta(Integer.valueOf(koko));
+		// laivat
+		shipsController.createLentotukialus(lta.getValue(), Integer.valueOf(koko));
+		shipsController.createTaistelulaiva(tl.getValue(), Integer.valueOf(koko));
+		shipsController.createRisteilija(r.getValue(), Integer.valueOf(koko));
+		shipsController.createSukellusvene(sv.getValue(), Integer.valueOf(koko));
+		shipsController.createHavittaja(h.getValue(), Integer.valueOf(koko));
+
+		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
 
 	}
-	
+
 }
--- src/main/java/fi/utu/tech/gui/javafx/setShipsController.java
+++ src/main/java/fi/utu/tech/gui/javafx/setShipsController.java
@@ -1,118 +1,112 @@
 package fi.utu.tech.gui.javafx;
 
 import java.io.IOException;
-import java.util.UUID;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Bounds;
 import javafx.geometry.Insets;
-import javafx.geometry.Point2D;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.input.Dragboard;
-import javafx.scene.input.TransferMode;
-import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
+public class setShipsController {
 
-public class setShipsController{
-	
-	//placeholder, joskus start game nappula, joka tulee kun molemmat laudat on asetettu ja peli voi alkaa
+	// placeholder, joskus start game nappula, joka tulee kun molemmat laudat on
+	// asetettu ja peli voi alkaa
 	@FXML
 	private Button nextscene;
 	@FXML
 	private boolean arePlaced;
 	@FXML
 	private VBox psIkkuna;
-	@FXML 
+	@FXML
 	private VBox psLauta;
-	@FXML 
+	@FXML
 	private StackPane laivaParkki;
-	
+
 	private Ship laiva;
-	
+
 	private Board lauta;
-	
+
 //	public void initialize() {
 //		//displayLauta(1);
 //		//displayShips();
 //	}
-	
-	
-	
-//	//saavat parametreina kuinka monta pitää tehdä ja koon
-	public void createLentotukialus(int maara, int lkoko){
-		for(int i=0; i<maara;i++) {
-			double ruutu=400/lkoko;
-		Ship laiva=new Ship(lauta);
-		laiva.setWidth(ruutu);
-		laiva.setHeight(5*ruutu);
-		
-		Bounds boundsInScene = laiva.localToScene(laiva.getBoundsInLocal());
-		System.out.println(boundsInScene);
-		laivaParkki.setAlignment(laiva,Pos.TOP_LEFT);
-		laivaParkki.setMargin(laiva,  new Insets(10,10,10,10) );
 
-		laivaParkki.getChildren().add(laiva);
+//	//saavat parametreina kuinka monta pitää tehdä ja koon
+	public void createLentotukialus(int maara, int lkoko) {
+		for (int i = 0; i < maara; i++) {
+			double ruutu = 400 / lkoko;
+			Ship laiva = new Ship(lauta);
+			laiva.setWidth(ruutu);
+			laiva.setHeight(5 * ruutu);
+
+			Bounds boundsInScene = laiva.localToScene(laiva.getBoundsInLocal());
+			System.out.println(boundsInScene);
+			laivaParkki.setAlignment(laiva, Pos.TOP_LEFT);
+			laivaParkki.setMargin(laiva, new Insets(10, 10, 10, 10));
+
+			laivaParkki.getChildren().add(laiva);
 		}
 	}
-public void createTaistelulaiva(int maara, int lkoko){
-	for(int i=0; i<maara;i++) {
-		double ruutu=400/lkoko;
-	Ship laiva=new Ship(lauta);
-	laiva.setWidth(ruutu);
-	laiva.setHeight(4*ruutu);
-	laivaParkki.setAlignment(laiva, Pos.TOP_CENTER);
-	laivaParkki.setMargin(laiva,  new Insets(10,10,10,10) );
-	laivaParkki.getChildren().add(laiva);
+
+	public void createTaistelulaiva(int maara, int lkoko) {
+		for (int i = 0; i < maara; i++) {
+			double ruutu = 400 / lkoko;
+			Ship laiva = new Ship(lauta);
+			laiva.setWidth(ruutu);
+			laiva.setHeight(4 * ruutu);
+			laivaParkki.setAlignment(laiva, Pos.TOP_CENTER);
+			laivaParkki.setMargin(laiva, new Insets(10, 10, 10, 10));
+			laivaParkki.getChildren().add(laiva);
+		}
 	}
+
+	public void createRisteilija(int maara, int lkoko) {
+		for (int i = 0; i < maara; i++) {
+			double ruutu = 400 / lkoko;
+			Ship laiva = new Ship(lauta);
+			laiva.setWidth(ruutu);
+			laiva.setHeight(3 * ruutu);
+			laivaParkki.setAlignment(laiva, Pos.TOP_RIGHT);
+			laivaParkki.setMargin(laiva, new Insets(10, 10, 10, 10));
+			laivaParkki.getChildren().add(laiva);
+		}
 	}
-public void createRisteilija(int maara, int lkoko){
-	for(int i=0; i<maara;i++) {
-		double ruutu=400/lkoko;
-	Ship laiva=new Ship(lauta);
-	laiva.setWidth(ruutu);
-	laiva.setHeight(3*ruutu);
-	laivaParkki.setAlignment(laiva, Pos.TOP_RIGHT);
-	laivaParkki.setMargin(laiva,  new Insets(10,10,10,10) );
-	laivaParkki.getChildren().add(laiva);
+
+	public void createSukellusvene(int maara, int lkoko) {
+		for (int i = 0; i < maara; i++) {
+			double ruutu = 400 / lkoko;
+			Ship laiva = new Ship(lauta);
+			laiva.setWidth(ruutu);
+			laiva.setHeight(3 * ruutu);
+			laivaParkki.setAlignment(laiva, Pos.BOTTOM_RIGHT);
+			laivaParkki.setMargin(laiva, new Insets(10, 10, 10, 10));
+			System.out.println("margin" + laivaParkki.getMargin(laiva));
+			laivaParkki.getChildren().add(laiva);
+		}
 	}
-}
-public void createSukellusvene(int maara, int lkoko){
-	for(int i=0; i<maara;i++) {
-		double ruutu=400/lkoko;
-	Ship laiva=new Ship(lauta);
-	laiva.setWidth(ruutu);
-	laiva.setHeight(3*ruutu);
-	laivaParkki.setAlignment(laiva, Pos.BOTTOM_RIGHT);
-	laivaParkki.setMargin(laiva,  new Insets(10,10,10,10) );
-	System.out.println("margin"+ laivaParkki.getMargin(laiva));
-	laivaParkki.getChildren().add(laiva);
+
+	public void createHavittaja(int maara, int lkoko) {
+		for (int i = 0; i < maara; i++) {
+			double ruutu = 400 / lkoko;
+			Ship laiva = new Ship(lauta);
+			laiva.setWidth(ruutu);
+			laiva.setHeight(2 * ruutu);
+			laivaParkki.setAlignment(laiva, Pos.BOTTOM_CENTER);
+			laivaParkki.setMargin(laiva, new Insets(10, 10, 10, 10));
+			laivaParkki.getChildren().add(laiva);
+		}
 	}
-}
-public void createHavittaja(int maara, int lkoko){
-	for(int i=0; i<maara;i++) {
-		double ruutu=400/lkoko;
-	Ship laiva=new Ship(lauta);
-	laiva.setWidth(ruutu);
-	laiva.setHeight(2*ruutu);
-	laivaParkki.setAlignment(laiva, Pos.BOTTOM_CENTER);
-	laivaParkki.setMargin(laiva,  new Insets(10,10,10,10) );
-	laivaParkki.getChildren().add(laiva);
-	}
-}
-	
+
 //	public void displayShips() {
 //		Ship laiva=new Ship(lauta);
 //		laiva.setWidth(50);
@@ -122,18 +116,16 @@ public void createHavittaja(int maara, int lkoko){
 //	
 //}
 	public void displayLauta(int koko) {
-		lauta=new Board(laivaParkki, koko);
-		
-		
-		
+		lauta = new Board(laivaParkki, koko);
 
 		psLauta.getChildren().add(lauta);
-		
+
 	}
+
 	@FXML
-	public void switchToBetweenScreen(ActionEvent event) throws IOException{
+	public void switchToBetweenScreen(ActionEvent event) throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("betweenScreen.fxml"));
-		Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		Scene scene = new Scene(root);
 		stage.setScene(scene);
 		stage.show();
@@ -142,9 +134,9 @@ public void createHavittaja(int maara, int lkoko){
 	@FXML
 	public void nextBoard(ActionEvent event) throws IOException {
 		if (arePlaced == true) {
-	//		SceneController.switchToBoardCreation(event);
-			
-		} 
+			// SceneController.switchToBoardCreation(event);
+
+		}
 	}
 
 	public Pane deleteShip(Ship ship) {
